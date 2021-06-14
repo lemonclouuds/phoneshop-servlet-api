a8a2596dfdae3af4274f709df66c1568e1f0fe4a
lemonclouds <hannah.lakhai@gmail.com>
2021-06-14 14:18:03 +0300

    Created cart page + serializable

---
+++ src/main/java/com/es/phoneshop/web/CartPageServlet.java
@@ -0,0 +1,72 @@
+package com.es.phoneshop.web;
+
+import com.es.phoneshop.model.product.ArrayListProductDao;
+import com.es.phoneshop.model.product.ProductDao;
+import com.es.phoneshop.model.product.ProductNotFoundException;
+import com.es.phoneshop.model.product.cart.Cart;
+import com.es.phoneshop.model.product.cart.CartService;
+import com.es.phoneshop.model.product.cart.DefaultCartService;
+import com.es.phoneshop.model.product.cart.OutOfStockException;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.NumberFormat;
+import java.text.ParseException;
+
+public class CartPageServlet extends HttpServlet {
+    private ProductDao productDao;
+    private CartService cartService;
+    private final String ERROR = "error";
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        productDao = ArrayListProductDao.getInstance();
+        cartService = DefaultCartService.getInstance();
+        //recentlyViewedService = DefaultRecentlyViewedService.getInstance();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setAttribute("cart", cartService.getCart(request));
+        request.getRequestDispatcher("/WEB-INF/pages/cart.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Long productId = parseProductId(request);
+        String quantityString = request.getParameter("quantity");
+        int quantity;
+        try {
+            NumberFormat format = NumberFormat.getInstance(request.getLocale());
+            quantity = format.parse(quantityString).intValue();
+        } catch (ParseException e) {
+            request.setAttribute(ERROR, "Not a number");
+            doGet(request, response);
+            return;
+        }
+
+        Cart cart = cartService.getCart(request);
+        try {
+            cartService.addProductToCart(cart, productId, quantity);
+        } catch (OutOfStockException e) {
+            request.setAttribute(ERROR, "Out of stock, available " + e.getStockAvailable());
+            doGet(request, response);
+            return;
+        }
+        response.sendRedirect(request.getContextPath() + "/products/" + productId + "?message=Product added to cart");
+    }
+
+    private Long parseProductId(HttpServletRequest request) {
+        String productInfo = request.getPathInfo().substring(1);
+        return Long.valueOf(productInfo);
+    }
+
+    public void setProductDao(ProductDao productDao){
+        this.productDao = productDao;
+    }
+}
